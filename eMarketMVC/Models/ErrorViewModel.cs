c06a081a5c7d901ce9f806a43ffa7cbc130984fe
QUANG TRUONG <TRUONGCHIQUANG@GMAIL.COM>
2022-05-21 22:42:47 -0500

    Create Projects

---
+++ eMarketMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eMarketMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
